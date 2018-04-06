499c9c9a05980049b1ebf06305cc243ccd3d4a7a
hd321kbps <hd321kbps@yandex.ru>
2018-04-06 21:19:42 +0300

    create plugin

---
+++ plugins/hd321kbps/blog/Plugin.php
@@ -0,0 +1,96 @@
+<?php namespace Hd321kbps\Blog;
+
+use Backend;
+use System\Classes\PluginBase;
+
+/**
+ * blog Plugin Information File
+ */
+class Plugin extends PluginBase
+{
+    /**
+     * Returns information about this plugin.
+     *
+     * @return array
+     */
+    public function pluginDetails()
+    {
+        return [
+            'name'        => 'blog',
+            'description' => 'No description provided yet...',
+            'author'      => 'hd321kbps',
+            'icon'        => 'icon-leaf'
+        ];
+    }
+
+    /**
+     * Register method, called when the plugin is first registered.
+     *
+     * @return void
+     */
+    public function register()
+    {
+
+    }
+
+    /**
+     * Boot method, called right before the request route.
+     *
+     * @return array
+     */
+    public function boot()
+    {
+
+    }
+
+    /**
+     * Registers any front-end components implemented in this plugin.
+     *
+     * @return array
+     */
+    public function registerComponents()
+    {
+        return []; // Remove this line to activate
+
+        return [
+            'Hd321kbps\Blog\Components\MyComponent' => 'myComponent',
+        ];
+    }
+
+    /**
+     * Registers any back-end permissions used by this plugin.
+     *
+     * @return array
+     */
+    public function registerPermissions()
+    {
+        return []; // Remove this line to activate
+
+        return [
+            'hd321kbps.blog.some_permission' => [
+                'tab' => 'blog',
+                'label' => 'Some permission'
+            ],
+        ];
+    }
+
+    /**
+     * Registers back-end navigation items for this plugin.
+     *
+     * @return array
+     */
+    public function registerNavigation()
+    {
+        return []; // Remove this line to activate
+
+        return [
+            'blog' => [
+                'label'       => 'blog',
+                'url'         => Backend::url('hd321kbps/blog/mycontroller'),
+                'icon'        => 'icon-leaf',
+                'permissions' => ['hd321kbps.blog.*'],
+                'order'       => 500,
+            ],
+        ];
+    }
+}
